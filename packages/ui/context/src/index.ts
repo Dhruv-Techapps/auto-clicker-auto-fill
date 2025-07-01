347f7666049a0e476caf11d35d45c55924815e4b
Dharmesh <dharmesh.hemaram@gmail.com>
2025-07-01 13:52:45 +0530

    removed theme context

--- packages/ui/context/src/index.ts
+++ packages/ui/context/src/index.ts
@@ -1,2 +1,1 @@
 export * from './lib/theme-context';
-export { type tTheme } from './lib/theme-context';
--- packages/ui/context/src/lib/theme-context.tsx
+++ packages/ui/context/src/lib/theme-context.tsx
@@ -1,47 +1,24 @@
 import React, { createContext, PropsWithChildren, useMemo, useState } from 'react';
 
-export type tTheme = 'light' | 'dark';
-
 export const ThemeContext = createContext({
   theme: localStorage.getItem('theme') || 'light',
-  updateTheme: (theme: tTheme | null) => {
-    return theme;
+  toggleTheme: () => {
+    // Default implementation for toggleTheme
   }
 });
 
-export const getStoredTheme = () => localStorage.getItem('theme');
-
-const removeStoredTheme = () => localStorage.removeItem('theme');
-
-const setStoredTheme = (theme: tTheme) => localStorage.setItem('theme', theme);
-
-const getPreferredTheme = (): tTheme => {
-  const storedTheme = getStoredTheme();
-  if (storedTheme) {
-    return storedTheme as tTheme;
-  }
-  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-};
-
 export const ThemeProvider: React.FC<PropsWithChildren> = ({ children }) => {
-  const [theme, setTheme] = useState<tTheme>(getPreferredTheme());
-
-  const updateTheme = (theme: tTheme | null) => {
-    if (theme === null) {
-      removeStoredTheme();
-      setTheme(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
-      document.documentElement.setAttribute('data-bs-theme', window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
-    } else {
-      setStoredTheme(theme);
-      setTheme(theme);
-      document.documentElement.setAttribute('data-bs-theme', theme);
-    }
-    return theme;
-  };
-
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
   document.documentElement.setAttribute('data-bs-theme', theme);
 
-  const value = useMemo(() => ({ theme, updateTheme }), [theme]);
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    document.documentElement.setAttribute('data-bs-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+
+  const value = useMemo(() => ({ theme, toggleTheme }), [theme]);
 
   return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>;
 };
