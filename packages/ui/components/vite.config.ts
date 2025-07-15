0703c3025e3dae6af16ce090fe1d86af25d81d99
Dharmesh <dharmesh.hemaram@gmail.com>
2025-07-15 09:01:38 +0530

    Fixed lint error

--- packages/ui/components/vite.config.ts
+++ packages/ui/components/vite.config.ts
@@ -15,9 +15,11 @@ export default defineConfig(() => ({
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
+    passWithNoTests: true,
     coverage: {
+      reporter: ['lcov'],
       reportsDirectory: './test-output/vitest/coverage',
-      provider: 'v8' as const
+      provider: 'istanbul' as const
     }
   }
 }));
