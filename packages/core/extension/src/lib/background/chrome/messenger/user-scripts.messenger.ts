bc16848c826d29e4a274085cf29003036ee9ea74
Dharmesh <dharmesh.hemaram@gmail.com>
2025-07-27 13:20:56 +0530

    Add user scripts availability check and notification handling

--- packages/core/extension/src/lib/background/chrome/messenger/user-scripts.messenger.ts
+++ packages/core/extension/src/lib/background/chrome/messenger/user-scripts.messenger.ts
@@ -1,4 +1,5 @@
 import { IExtension } from '@dhruv-techapps/core-common';
+import { NotificationsMessenger } from './notifications.messenger';
 
 export interface IUserScriptsRequest {
   messenger: 'userScripts';
@@ -16,8 +17,39 @@ export interface IUserScriptsExecuteResponse {
   error?: string;
 }
 
+async function isUserScriptsAvailable() {
+  try {
+    // Method call which throws if API permission or toggle is not enabled.
+    await chrome.userScripts.getScripts();
+    return true;
+  } catch {
+    // Not available.
+    return false;
+  }
+}
+
+function Notify() {
+  const { action } = chrome.runtime.getManifest();
+  const defaultOptions: chrome.notifications.NotificationCreateOptions = {
+    type: 'basic',
+    iconUrl: action.default_icon,
+    title: 'Allow user scripts',
+    message: 'User scripts API is not available. Please enable it in the extension settings.',
+    requireInteraction: true,
+    silent: false
+  };
+  new NotificationsMessenger().create({
+    notificationId: 'user-scripts-not-available',
+    options: defaultOptions
+  });
+}
+
 export class UserScriptsMessenger {
   async execute({ code, ext }: IUserScriptsExecuteProps, sender: chrome.runtime.MessageSender): Promise<IUserScriptsExecuteResponse> {
+    if (!(await isUserScriptsAvailable())) {
+      Notify();
+      throw new Error('User scripts API is not available. Ensure the extension has the necessary permissions.');
+    }
     const tabId = sender.tab?.id;
     if (typeof tabId !== 'number') {
       throw new Error('Tab ID is not defined or invalid');
