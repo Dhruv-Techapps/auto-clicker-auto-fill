8b29085efa247583b2313b493539105190bfe7ef
Dharmesh <dharmesh.hemaram@gmail.com>
2026-01-29 18:51:57 +0530

    fix: simplify error logging for setting uninstall URL

--- apps/acf-extension/src/background/chrome.runtime.set-uninstall-url.ts
+++ apps/acf-extension/src/background/chrome.runtime.set-uninstall-url.ts
@@ -4,11 +4,7 @@ if (UNINSTALL_URL) {
   try {
     chrome.runtime.setUninstallURL(UNINSTALL_URL, () => {
       if (chrome.runtime.lastError) {
-        console.error(
-          'Failed to set uninstall URL:',
-          UNINSTALL_URL,
-          chrome.runtime.lastError
-        );
+        console.error('Failed to set uninstall URL:', UNINSTALL_URL, chrome.runtime.lastError);
       } else {
         console.info('Successfully set uninstall URL:', UNINSTALL_URL);
       }
