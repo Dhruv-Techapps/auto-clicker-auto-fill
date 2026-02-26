f4c01fdad8e6bacd7cca66389e7ed4d2c8092154
Dharmesh <dharmesh.hemaram@gmail.com>
2026-02-26 10:16:32 +0530

    refactor: improve formatting of HTML and simplify tooltip component return statement

--- apps/acf-options-page/src/tooltip/value-field-type.tooltip.tsx
+++ apps/acf-options-page/src/tooltip/value-field-type.tooltip.tsx
@@ -7,9 +7,5 @@ interface ValueFieldTypeTooltipProps {
 }
 
 export function ValueFieldTypeTooltip({ id, valueFieldType, children }: ValueFieldTypeTooltipProps) {
-  return (
-    <OverlayTrigger overlay={<Tooltip id={id}>{valueFieldType === 'input' ? 'Switch to Textarea' : 'Switch to Input'}</Tooltip>}>
-      {children}
-    </OverlayTrigger>
-  );
+  return <OverlayTrigger overlay={<Tooltip id={id}>{valueFieldType === 'input' ? 'Switch to Textarea' : 'Switch to Input'}</Tooltip>}>{children}</OverlayTrigger>;
 }
