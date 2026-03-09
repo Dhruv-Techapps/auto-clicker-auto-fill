73674b4ac5e198a8af84eefe15962cb67f4cc046
Dharmesh <dharmesh.hemaram@gmail.com>
2026-03-09 17:06:38 +0530

    chore: update package dependencies and versions
    - Changed "@nx/cypress" to "@nx/devkit" version 22.5.4
    - Updated "@nx/playwright" to use version range "^22.5.4"
    - Removed fixed version for "cypress" (15.8.1)
    - Added "eslint-plugin-playwright" with version "^1.6.2"
    - Removed fixed version for "eslint-plugin-cypress" (3.5.0)

---
+++ apps/acf-options-page/e2e/fixtures/extension.ts
@@ -0,0 +1,37 @@
+import { workspaceRoot } from '@nx/devkit';
+import { test as base, chromium, expect } from '@playwright/test';
+import * as path from 'path';
+
+const extensionPath = path.join(workspaceRoot, 'apps/acf-extension/dist');
+
+export { expect };
+
+export const test = base.extend<{
+  extensionId: string;
+}>({
+  // Override the default context to use a persistent context with the extension loaded
+  context: async ({}, use) => {
+    const context = await chromium.launchPersistentContext('', {
+      headless: false,
+      args: [`--disable-extensions-except=${extensionPath}`, `--load-extension=${extensionPath}`]
+    });
+    await use(context);
+    await context.close();
+  },
+
+  // Resolve the extension ID from the background service worker
+  extensionId: async ({ context }, use) => {
+    let worker = context.serviceWorkers()[0];
+    if (!worker) {
+      worker = await context.waitForEvent('serviceworker');
+    }
+    const extensionId = worker.url().split('/')[2];
+    await use(extensionId);
+  },
+
+  // Provide a page from the persistent context
+  page: async ({ context }, use) => {
+    const page = await context.newPage();
+    await use(page);
+  }
+});
