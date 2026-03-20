a53de6e54ee393d5558f3e265d1057e99a4128b6
Dharmesh <dharmesh.hemaram@gmail.com>
2026-03-20 21:56:31 +0530

    format

--- apps/acf-extension/webpack.config.js
+++ apps/acf-extension/webpack.config.js
@@ -12,7 +12,7 @@ function modify(buffer, { KEY, VITE_PUBLIC_NAME, OAUTH_CLIENT_ID, VITE_PUBLIC_RE
   manifest.version = VITE_PUBLIC_RELEASE_VERSION.replace('v', '');
   manifest.name = VITE_PUBLIC_NAME;
   if (VITE_PUBLIC_URL) {
-    manifest.externally_connectable.matches.push(VITE_PUBLIC_URL);
+    manifest.externally_connectable.matches.push(VITE_PUBLIC_URL + '/*');
   }
   if (OAUTH_CLIENT_ID) {
     manifest.oauth2.client_id = OAUTH_CLIENT_ID;
